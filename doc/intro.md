272ff79c9009baff8f2bff2edc365f99d4893787
Anton Strömkvist <hi@ahst.ro>
2016-04-19 22:23:27 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dribbble-scraper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dribbble_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns dribbble-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [dribbble-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
